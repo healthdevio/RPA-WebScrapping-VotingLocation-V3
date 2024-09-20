f124f02facf4334bc7c03e5e46fefce4a461e70b
“odevnicolas” <nickolas@s4s.tech>
2024-09-20 16:39:38 -0300

    falta abrir o browser

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const { Cluster } = require('puppeteer-cluster');
-const os = require('os');
-const cluster = require('cluster');
-require('dotenv').config();
+const puppeteer = require('puppeteer');
 const { Client } = require('pg');
 const { normalize } = require('./normalize');
 const fs = require('fs');
 const path = require('path');
 
+// Conexão direta com o banco de dados usando dados fornecidos
+const dbConfig = {
+  user: 'zerooitocincoadm',
+  host: 'zerooitocincodb.c5qnvtxpcnuu.us-east-1.rds.amazonaws.com',
+  database: '085db',
+  password: 'GrzYTKeT3ZmSg4JfpnVqUYSd',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Ignora erros de certificado
+  },
+};
+
+// Função para formatar a data
 function formatDate(dateStr) {
   const [day, month, year] = dateStr.split('/');
   return `${day}/${month}/${year}`;
 }
 
+// Função para calcular a idade
 function calculateAge(birthDate) {
   const [day, month, year] = birthDate.split('/');
   const birth = new Date(`${year}-${month}-${day}`);
@@ -20,18 +31,9 @@ function calculateAge(birthDate) {
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
+// Função para buscar dados do banco de dados
 async function getPeopleFromDatabase(offset = 0, limit = 1000) {
-  const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-
+  const client = new Client(dbConfig);
   await client.connect();
   console.log(`Executando consulta SQL com offset ${offset} e limite ${limit}`);
 
@@ -43,31 +45,19 @@ async function getPeopleFromDatabase(offset = 0, limit = 1000) {
   `;
 
   const res = await client.query(query);
-  console.log(`Dados retornados do banco de dados: ${JSON.stringify(res.rows, null, 2)}`);
   await client.end();
 
   const filteredPeople = res.rows.filter((person) => {
     const age = calculateAge(person.original_birth_date);
-    console.log(`Calculando idade para ${person.name}: ${age} anos`);
     return age >= 16 && age <= 30;
   });
 
-  console.log(`Pessoas filtradas entre 16 e 30 anos: ${JSON.stringify(filteredPeople, null, 2)}`);
   return filteredPeople;
 }
 
+// Função para atualizar dados do eleitor no banco de dados
 async function updatePersonVoterData(personId, voterData) {
-  const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-
+  const client = new Client(dbConfig);
   await client.connect();
   console.log(`Atualizando dados de votação para a pessoa ID: ${personId}`);
 
@@ -104,219 +94,140 @@ async function updatePersonVoterData(personId, voterData) {
   }
 }
 
-async function createPuppeteerCluster() {
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_BROWSER,
-    maxConcurrency: 1, 
-    puppeteerOptions: {
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--disable-gpu',
-        '--disable-extensions',
-      ],
-    },
-    monitor: true,
-    timeout: 180000,  
+// Função principal que cria o browser e executa o processo de scraping
+async function executeVoterDataProcess(person) {
+  const { id, name, original_birth_date: birthDate, mother_name: motherName } = person;
+
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--disable-gpu',
+      '--incognito',
+    ],
+    headless: true
   });
 
-  await cluster.task(async ({ page, data: { person, counts } }) => {
-    const { id, name, original_birth_date: birthDate, mother_name: motherName } = person;
+  const page = await browser.newPage();
 
-    try {
-      console.log(`Processando ${name}...`);
+  try {
+    await page.goto('https://www.tre-ce.jus.br/servicos-eleitorais/titulo-e-local-de-votacao/consulta-por-nome', {
+      waitUntil: 'domcontentloaded',
+      timeout: 120000,
+    });
 
-      await page.goto('https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral#/atendimento-eleitor/onde-votar', {
-        waitUntil: 'networkidle2',
-        timeout: 120000,
-      });
+    // Fechar pop-up de cookies
+    await page.waitForSelector('.cookies .botao button', { visible: true, timeout: 5000 });
+    const cienteButton = await page.$('div.botao button.btn');
+    if (cienteButton) await cienteButton.click();
 
-      await page.waitForSelector('.cookies .botao button', { visible: true, timeout: 5000 });
-      const cienteButton = await page.$('div.botao button.btn');
-      if (cienteButton) {
-        await cienteButton.click();
-        console.log('Pop-up de cookies fechado.');
-      } else {
-        console.log('Botão "Ciente" não encontrado.');
-      }
+    const formattedBirthDate = formatDate(birthDate);
+    const normalizedName = normalize(name.toUpperCase());
+    const normalizedMotherName = normalize(motherName.toUpperCase());
 
-      const formattedBirthDate = formatDate(birthDate);
-      const normalizedName = normalize(name.toUpperCase());
-      const normalizedMotherName = normalize(motherName.toUpperCase());
+    // Preencher os campos do formulário
+    await page.type('input[placeholder="Número do título eleitoral ou CPF ou nome"]', normalizedName);
+    await page.type('input[placeholder="Data de nascimento (dia/mês/ano)"]', formattedBirthDate);
+    await page.type('[formcontrolname=nomeMae]', normalizedMotherName);
 
-      await page.type('input[placeholder="Número do título eleitoral ou CPF ou nome"]', normalizedName);
-      console.log(`Nome preenchido: ${normalizedName}`);
-
-      await page.type('input[placeholder="Data de nascimento (dia/mês/ano)"]', formattedBirthDate);
-      console.log(`Data de nascimento preenchida: ${formattedBirthDate}`);
-
-      await page.type('[formcontrolname=nomeMae]', normalizedMotherName);
-      console.log(`Nome da mãe preenchido: ${normalizedMotherName}`);
-
-      const submitButton = await page.$('button[type="submit"]');
-      if (submitButton) {
-        await page.evaluate(button => button.click(), submitButton);
-        console.log(`Formulário submetido para ${name}`);
-      } else {
-        throw new Error('Botão de submissão não encontrado');
-      }
-
-      await page.waitForFunction(() => !document.body.innerText.includes('carregando conteúdo'), { timeout: 120000 });
-
-      if ((await page.$('div.alert.alert-warning')) !== null) {
-        console.log(`Pessoa não encontrada no sistema do TSE: ${name}`);
-        counts.failure++;
-        return { error: 'Pessoa não encontrada' };
-      }
-
-      // Adiciona espera para garantir que o conteúdo foi carregado
-      await page.waitForTimeout(3000); 
-
-      const data = await page.evaluate(() => {
-        const voterComponent = document.querySelector('.componente-onde-votar');
-        if (!voterComponent) {
-          return {
-            error: true,
-            message: 'Pessoa não encontrada no sistema do TSE',
-          };
-        }
-
-        const labels = Array.from(
-          document.querySelectorAll('.lado-ov .data-box .label'),
-        ).map((el) => el.textContent.trim() ?? null);
-
-        const descs = Array.from(
-          document.querySelectorAll('.lado-ov .data-box .desc'),
-        ).map((el) => el.textContent.trim() ?? null);
-
-        const result = {};
-        const possibleLabels = {
-          'Local de votação': 'local',
-          'Endereço': 'endereco',
-          'Município/UF': 'municipio',
-          'Bairro': 'bairro',
-          'Seção': 'secao',
-          'Zona': 'zona',
-        };
-
-        labels.forEach((label, i) => {
-          const key = possibleLabels[label];
-          if (key) {
-            result[key] = descs[i] || null;
-          }
-        });
-
-        result.biometria = document.body.innerText.includes(
-          'ELEITOR/ELEITORA COM BIOMETRIA COLETADA'
-        );
-
-        return { error: false, data: result };
-      });
-
-      if (data.error) {
-        const screenshotPath = path.join(
-          __dirname,
-          'rpa',
-          `pessoa_nao_encontrada_${name.replace(/\s+/g, '_')}.png`,
-        );
-        await page.screenshot({ path: screenshotPath });
-        throw new Error(data.message);
-      }
-      
-      console.log(`Dados encontrados com sucesso: ${JSON.stringify(data.data)}`);
-      counts.success++;
-      await updatePersonVoterData(id, data.data);
-      return data.data;
-    } catch (error) {
-      console.error(`Erro ao processar ${name}: ${error.message}`);
-      console.log('Dados do caba mae, nome, data:', motherName, name, birthDate); 
-      await page.screenshot({ path: `error_${name}.png`, fullPage: true }); 
-      counts.failure++;
-      return { error: error.message };
+    // Submeter o formulário
+    const submitButton = await page.$('button[type="submit"]');
+    if (submitButton) {
+      await page.evaluate(button => button.click(), submitButton);
+    } else {
+      throw new Error('Botão de submissão não encontrado');
     }
-  });
 
-  return cluster;
+    // Espera para o carregamento do conteúdo
+    await page.waitForFunction(() => !document.body.innerText.includes('carregando conteúdo'), { timeout: 120000 });
+
+    if ((await page.$('div.alert.alert-warning')) !== null) {
+      console.log(`Pessoa não encontrada no sistema do TSE: ${name}`);
+      return { error: 'Pessoa não encontrada' };
+    }
+
+    // Aguardar tempo adicional para garantir o carregamento
+    await page.waitForTimeout(3000);
+
+    // Extrair dados da página
+    const data = await page.evaluate(() => {
+      const voterComponent = document.querySelector('.componente-onde-votar');
+      if (!voterComponent) {
+        return {
+          error: true,
+          message: 'Pessoa não encontrada no sistema do TSE',
+        };
+      }
+
+      const labels = Array.from(
+        document.querySelectorAll('.lado-ov .data-box .label'),
+      ).map((el) => el.textContent.trim() ?? null);
+
+      const descs = Array.from(
+        document.querySelectorAll('.lado-ov .data-box .desc'),
+      ).map((el) => el.textContent.trim() ?? null);
+
+      const result = {};
+      const possibleLabels = {
+        'Local de votação': 'local',
+        'Endereço': 'endereco',
+        'Município/UF': 'municipio',
+        'Bairro': 'bairro',
+        'Seção': 'secao',
+        'Zona': 'zona',
+      };
+
+      labels.forEach((label, i) => {
+        const key = possibleLabels[label];
+        if (key) {
+          result[key] = descs[i] || null;
+        }
+      });
+
+      result.biometria = document.body.innerText.includes(
+        'ELEITOR/ELEITORA COM BIOMETRIA COLETADA'
+      );
+
+      return { error: false, data: result };
+    });
+
+    if (data.error) {
+      const screenshotPath = path.join(
+        __dirname,
+        'rpa',
+        `pessoa_nao_encontrada_${name.replace(/\s+/g, '_')}.png`,
+      );
+      await page.screenshot({ path: screenshotPath });
+      throw new Error(data.message);
+    }
+
+    console.log(`Dados encontrados com sucesso: ${JSON.stringify(data.data)}`);
+    await updatePersonVoterData(id, data.data);
+    return data.data;
+
+  } catch (error) {
+    console.error(`Erro ao processar ${name}: ${error.message}`);
+    const screenshotPath = path.join(__dirname, 'rpa', `error_${name}.png`);
+    await page.screenshot({ path: screenshotPath });
+    throw error;
+  } finally {
+    await page.close();
+    await browser.close();
+  }
 }
 
+// Executar o processo para buscar pessoas do banco de dados e processar
 (async function () {
-  if (cluster.isMaster) {
-    const numCPUs = os.cpus().length;
-    const batchSize = 1000;
-    let offset = 0;
-    let totalProcessed = 0;
-    let totalSuccess = 0;
-    let totalFailure = 0;
-    let noResultsCounter = 0;
-
-    while (noResultsCounter < 3) {
-      console.log(`Buscando pessoas no banco de dados com offset ${offset} e batchSize ${batchSize}`);
-      const people = await getPeopleFromDatabase(offset, batchSize);
-
-      if (people.length === 0) {
-        console.log('Nenhuma pessoa foi encontrada no banco de dados.');
-        noResultsCounter++;
-        offset += batchSize;
-        continue;
-      }
-
-      console.log(`Iniciando processamento do batch com ${people.length} pessoas.`);
-      noResultsCounter = 0;
-
-      const workers = [];
-      for (let i = 0; i < numCPUs; i++) {
-        const worker = cluster.fork();
-        workers.push(worker);
-
-        worker.send({
-          people: people.slice(i * Math.ceil(people.length / numCPUs), (i + 1) * Math.ceil(people.length / numCPUs)),
-          counts: { success: 0, failure: 0 }
-        });
-
-        worker.on('message', ({ success, failure }) => {
-          totalSuccess += success;
-          totalFailure += failure;
-          totalProcessed += success + failure;
-          console.log(`Worker ${worker.id} finalizou o processamento: ${success} com sucesso, ${failure} falharam.`);
-        });
-
-        worker.on('exit', (code, signal) => {
-          console.log(`Worker ${worker.process.pid} foi encerrado. Código: ${code}, Sinal: ${signal}`);
-        });
-      }
-
-      for (const worker of workers) {
-        await new Promise((resolve) => worker.on('exit', resolve));
-        worker.kill();
-      }
-
-      offset += batchSize;
+  const people = await getPeopleFromDatabase();
+  for (const person of people) {
+    try {
+      await executeVoterDataProcess(person);
+    } catch (error) {
+      console.error(`Erro no processamento do RPA para ${person.name}: ${error.message}`);
     }
-
-    console.log(`Processamento completo. Total de registros processados: ${totalProcessed}`);
-    console.log(`Total com sucesso: ${totalSuccess}, total com falha: ${totalFailure}`);
-  } else {
-    process.on('message', async ({ people, counts }) => {
-      const puppeteerCluster = await createPuppeteerCluster();
-
-      try {
-        for (const person of people) {
-          puppeteerCluster.queue({ person, counts });
-        }
-
-        await puppeteerCluster.idle();
-      } catch (error) {
-        console.error('Erro durante o processamento:', error);
-      } finally {
-        await puppeteerCluster.close();
-        process.send({ success: counts.success, failure: counts.failure });
-        process.exit(); 
-      }
-    });
   }
 })();
